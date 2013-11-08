a49c7ac54a410069bd56cd093170fc047c72e0e3
Dag Stuan <dag.stuan@bekk.no>
2013-11-08 14:04:16 +0100

    independent drawing of bees

--- bee.lua
+++ bee.lua
@@ -1,16 +1,19 @@
-bee = {}
+bee = {
+  image = nil, -- see note below
+  name = nil
+}
 
-function bee:new(display)
-    local instance = {}
-    setmetatable(instance, self)
-    self.__index = self
-    self.image = display.newImage("images/bee_1.png")
-    self.image.x = 20
-    self.image.y = 20
-    
-    return bee
-end
-
-function bee:draw(self)
-    self.image.x = self.image.x + 1
+function bee.Create(display)
+  -- first make a copy of the master object
+  local self = {}
+  -- then set fields
+  self.image = display.newImage("images/bee_1.png")
+  self.image.x = 20
+  self.image.y = 20
+  
+  self.Draw = function()
+      self.image.x = self.image.x + 1
+  end
+  
+  return self 
 end
