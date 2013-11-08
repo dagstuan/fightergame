5d7293b7b62cb7d70b0d3db812ebbe8d0553984b
Dag Stuan <dag.stuan@bekk.no>
2013-11-08 10:51:24 +0100

    Movement

--- main.lua
+++ main.lua
@@ -19,18 +19,60 @@ local sky = display.newImage( "sky.png", true )
 game:insert( sky )
 sky.x = 120; sky.y = 200
 
+local grass = display.newImage( "grass.png", true )
+game:insert( grass )
+grass.x = 160
+grass.y = 500
+
 ------------------------------------------------------------
 -- Launch boulder
 local boulder = display.newImage( "boulder.png" )
-boulder.x = display.contentWidth/2
+boulder.x = display.contentWidth /2
 boulder.y = display.contentHeight - boulder.height / 2
 game:insert( boulder )
 
--- Camera follows bolder automatically
-local function moveCamera()
-	if (boulder.x > 180 and boulder.x < 1100) then
-		game.x = -boulder.x + 180
-	end
+------------------------------------------------------------
+-- Move the ship
+local holding = false
+local left = false
+local event = null
+
+function holdingEvent()
+    if (pressed) then
+        if (left) then
+            if (boulder.x < display.contentWidth - boulder.width / 2) then
+                boulder.x = boulder.x + 15
+            end
+        else
+            if (boulder.x > boulder.width / 2) then
+                boulder.x = boulder.x - 15
+            end
+        end
+    end
 end
 
-Runtime:addEventListener( "enterFrame", moveCamera )
+function touch( event )
+    if event.phase == "began" then
+        pressed = true
+        if (event.x > 120 ) then
+            left = true
+        else
+            left = false
+        end
+        Runtime:addEventListener( "enterFrame", holdingEvent )
+    elseif event.phase == "ended" then
+        pressed = false
+        Runtime:removeEventListener( "enterFrame", holdingEvent )
+    end
+    return true
+end
+
+Runtime:addEventListener( "touch", touch )
+
+
+------------------------------------------------------------
+-- Left button
+
+
+------------------------------------------------------------
+-- Right button
